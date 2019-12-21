b58c452ccdceee4d89043a82ab94eebcf0d7c13a
sandeep niture <sandeepvnm@gmail.com>
2019-12-21 15:20:23 +0530

    demo

---
+++ demo/src/main/java/com/tet/microservice/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.tet.microservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
